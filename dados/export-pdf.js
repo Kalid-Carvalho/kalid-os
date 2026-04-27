23643cf3c0069fceb4c87d7b27c01c82d1cee5ce
Kalid Carvalho da Silva <kalid.carvalho12@outlook.com>
2026-04-27 11:55:42 -0400

    auto-sync: 2026-04-27 11:55

--- dados/export-pdf.js
+++ dados/export-pdf.js
@@ -14,7 +14,7 @@ const { chromium } = require('playwright');
     format: 'A4',
     printBackground: true,
     margin: { top: '0', right: '0', bottom: '0', left: '0' },
-    scale: 1
+    scale: 0.75
   });
 
   console.log('done');
